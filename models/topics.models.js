0e1ea8a2433f7f4f2171d85fa4f9a9749dd1935f
ajknowlden1 <ajknowlden1@gmail.com>
2022-05-17 12:24:36 +0100

    updates

--- models/topics.models.js
+++
@@ -1,6 +0,0 @@
-const db = require("../db/connection");
-const selectTopics = () => {
-  return db.query(`SELECT * FROM topics`).then((result) => result.rows);
-};
-
-module.exports = { selectTopics };
