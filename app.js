ae63cb2b9870d7dea41758ce0e1e79359606842f
ajknowlden1 <ajknowlden1@gmail.com>
2022-05-24 22:58:04 +0100

    cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { getTopics } = require("./controllers/topics.controllers");
 const { getAllEndpoints } = require("./controllers/api.controllers");
 const { getUsers } = require("./controllers/users.controllers");
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use(express.json());
 
