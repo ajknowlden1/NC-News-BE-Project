62cc7d8ccb44602e5cca998bd6040d11ce20f92b
ajknowlden1 <ajknowlden1@gmail.com>
2022-06-16 16:31:26 +0100

    fixes comment order

--- models/articles.models.js
+++ models/articles.models.js
@@ -72,7 +72,7 @@ const selectArticleComments = (id) => {
       else
         return db
           .query(
-            `SELECT comment_id, body, author, votes, created_at FROM comments WHERE comments.article_id = $1`,
+            `SELECT comment_id, body, author, votes, created_at FROM comments WHERE comments.article_id = $1 ORDER BY created_at DESC`,
             [id]
           )
           .then((result) => {
